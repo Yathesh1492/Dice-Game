378a0a1f7ad8c8a0d98cdbac8dc3a64fb59df5cf
Yathesh <102244855+Yathesh1492@users.noreply.github.com>
2023-05-20 11:35:09 +0530

    Update index.js

--- Dice Game/index.js
+++ Dice Game/index.js
@@ -1,17 +1,16 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-var randomDiceImage = "dice-face-" + randomNumber1 + ".png"; 
-var randomImageSource = "images/" + randomDiceImage; 
+var randomPlayer1 = Math.floor(Math.random() * 6) + 1; 
+var randomImage = "images/dice-face-" + randomPlayer1 + ".png";
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice-face-" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Win the game";
+image1.setAttribute("src", randomImage);
+var randomPlayer2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice-face-" + randomPlayer2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+if (randomPlayer1 > randomPlayer2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins the game";
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Win the game";
+else if (randomPlayer1 < randomPlayer2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins the game";
 }
 else {
-  document.querySelector("h1").innerHTML = "Tie!";
+  document.querySelector("h1").innerHTML = "Game is Tie!";
 }
